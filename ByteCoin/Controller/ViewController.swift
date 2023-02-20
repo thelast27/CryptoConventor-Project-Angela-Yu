c7962e8a42a6b7c3569b83a0124504d9f7b3785e
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2023-02-20 13:36:09 +0300

    set ui picker and link with currency label

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var currentCurrencySelected: UILabel!
+    var coinManager = CoinManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
 
 
 }
 
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        
+        // The number of rows of data
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return coinManager.currencyArray.count
+        }
+        
+        // The data to return fopr the row and component (column) that's being passed in
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            currentCurrencySelected.text = coinManager.currencyArray[row]
+            return coinManager.currencyArray[row]
+        }
+}
