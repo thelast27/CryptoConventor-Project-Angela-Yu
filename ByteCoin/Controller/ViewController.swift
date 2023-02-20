1e99f06dd9dcadb40b00dfe742713eae2cab2522
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2023-02-20 13:51:52 +0300

    fix a little

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  
+    
+    
     @IBOutlet weak var currencyPicker: UIPickerView!
     @IBOutlet weak var currentCurrencySelected: UILabel!
     var coinManager = CoinManager()
@@ -21,24 +21,22 @@ class ViewController: UIViewController {
         currencyPicker.delegate = self
         currencyPicker.dataSource = self
     }
-
-
+    
+    
 }
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return 1
-        }
-        
-        // The number of rows of data
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            return coinManager.currencyArray.count
-        }
-        
-        // The data to return fopr the row and component (column) that's being passed in
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-            currentCurrencySelected.text = coinManager.currencyArray[row]
-            return coinManager.currencyArray[row]
-        }
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        currentCurrencySelected.text = coinManager.currencyArray[row]
+        return coinManager.currencyArray[row]
+    }
 }
